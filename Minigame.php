56c44ca522934ef54274bb5d2f194233c544fee8
RageOfFire <buihongson453@gmail.com>
2021-12-19 17:41:44 +0700

    Fix database

--- Minigame.php
+++ Minigame.php
@@ -21,7 +21,21 @@
     else {
       $url = $_REQUEST['url']."&type=".$_REQUEST['type'];
     }
-    $get = file_get_contents($url);
+    function file_get_contents_ssl($url) {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_REFERER, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000); // 3 sec.
+      curl_setopt($ch, CURLOPT_TIMEOUT, 10000); // 10 sec.
+      $result = curl_exec($ch);
+      curl_close($ch);
+      return $result;
+  }
+    $get = file_get_contents_ssl($url);
     $json = json_decode($get);
     $i = 0;
     foreach ($json->results as $question) {
--- assets/database/kingsoundtrackmp3.sql
+++ assets/database/kingsoundtrackmp3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2021 at 05:20 AM
+-- Generation Time: Dec 19, 2021 at 11:40 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
