b6d03e71dc1dd1a7b6706561b80da5add01d4354
RageOfFire <buihongson453@gmail.com>
2022-02-08 15:36:16 +0700

    add 404 not found page

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 page not found</title>
+    <?php include './assets/include/framework.php';?>
+</head>
+<body>
+<script>
+  window.onload = function() {
+  Swal.fire({
+      icon: 'question',
+      title: '404 not found',
+      text: 'Không tìm thấy trang ! Chuyển hướng về trang chủ',
+      showConfirmButton: false,
+      backdrop: 'gray',
+      timer: 2000,
+      timerProgressBar: true
+    }).then(function() {location.href= "./";})
+  }
+  </script>
+</body>
+</html>
