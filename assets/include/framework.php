92cfc4acb45106d68fcb76345563c2445673572a
RageOfFire <buihongson453@gmail.com>
2022-02-11 17:00:15 +0700

    update description

--- assets/include/framework.php
+++ assets/include/framework.php
@@ -1,9 +1,10 @@
+<meta name="description" content="KingsoundtrackMP3 là 1 trang web cho phép bạn nghe nhạc và chia sẻ những bản nhạc bạn yêu thích cho mọi người">
 <meta name="og:title" content="KingSoundTrackMP3" />
 <meta name="og:url" content="https://www.kingsoundtrackmp3.tk" />
 <meta name="og:image" content="https://raw.githubusercontent.com/RageOfFire/KingSoundTrackMP3/master/assets/img/1546787190_Music-music-1440-900.jpg" />
 <meta name="og:image:width" content="1280" />
 <meta name="og:image:height" content="720" />
-<meta name="og:description" content="Nơi bạn nghe nhạc và thư giãn" />
+<meta name="og:description" content="KingsoundtrackMP3 là 1 trang web cho phép bạn nghe nhạc và chia sẻ những bản nhạc bạn yêu thích cho mọi người" />
 <meta name="theme-color" content="#ff9933">
 <link rel="shortcut icon" href="./assets/img/avatars-000606604806-j6ghpm-t500x500.jpg" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
