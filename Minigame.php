9af2e9171f5742214ea85940e70fdafc7caf9c57
RageOfFire <buihongson453@gmail.com>
2022-05-19 23:22:08 +0700

    update readme

---
+++ Minigame.php
@@ -0,0 +1,78 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Minigame</title>
+  <?php include './assets/include/framework.php'; ?>
+  <link rel="stylesheet" href="./assets/css/editprofile.css">
+</head>
+<body>
+<?php include './assets/include/header.php'; ?>
+<?php include './assets/include/check.php'; ?>
+<?php include './assets/include/check-invalid-user.php'; ?>
+  <main>
+    <hr class="mb-3">
+    <form action="./assets/PHP/getcoin.php" method="POST">
+    <?php
+    if (isset($_REQUEST['category'])) {
+      $url = $_REQUEST['url']."&category=".$_REQUEST['category']."&type=".$_REQUEST['type'];
+    }
+    else {
+      $url = $_REQUEST['url']."&type=".$_REQUEST['type'];
+    }
+    function file_get_contents_ssl($url) {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_REFERER, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000); // 3 sec.
+      curl_setopt($ch, CURLOPT_TIMEOUT, 10000); // 10 sec.
+      $result = curl_exec($ch);
+      curl_close($ch);
+      return $result;
+  }
+    $get = file_get_contents_ssl($url);
+    $json = json_decode($get);
+    $i = 0;
+    foreach ($json->results as $question) {
+      $i++;
+      echo 
+      '
+      <div class="card mb-3 mt-3">
+      <div class="card-body">
+      <h4 class="text-success">Câu '.$i.': '.$question->question.'</h4>
+      <h5 class="text-success">Độ khó: '.$question->difficulty.'</h5>
+      <div class="form-check">
+      <input class="form-check-input" type="radio" name="choose_'.$i.'" id="true_'.$i.'" value="True" required>
+      <label class="form-check-label text-danger" for="true_'.$i.'">
+      Đúng
+      </label>
+      </div>
+      <div class="form-check">
+      <input class="form-check-input" type="radio" name="choose_'.$i.'" id="false_'.$i.'" value="False" required>
+      <label class="form-check-label text-danger" for="false_'.$i.'">
+      Sai
+      </label>
+      </div>
+      <input type="hidden" name="'.$i.'" value='.$question->correct_answer.'>
+      </div>
+      </div>
+      <hr>
+      ';
+    }
+    ?>
+    <div class="d-flex justify-content-end m-2">
+      <button type="submit" class="btn btn-success">Gửi kết quả</button>
+    </div>
+    </form>
+    <?php include "./assets/include/music-kit.php"; ?>
+  </main>
+  <?php include './assets/include/footer.php'; ?>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,17 +1,54 @@
 # KingSoundTrackMP3
-Đây là 1 website quản lý nhạc sử dụng PHP mà MySQL
-Website này có những chức năng sau:
-- Đăng ký tài khoản người dùng
-- Chỉnh sửa tài khoản
-- Thêm nhạc
-- Nghe nhạc
-- Chỉnh sửa nhạc
-- Xóa nhạc
-- Quản lý tài khoản,nhạc
-- Mingame
-- chuyển đổi youtube video sang dạng mp3
-- ...
-Hướng dẫn cách sử dụng:
-Tải xampp về và xuất file assets/database/kingsoundtrackmp3.sql vào phpmyadmin
-Sau đó mở file index.php để bắt đầu sử dụng
-Link Demo: https://kingsoundtrackmp3.tk/
+
+* Đây là 1 website quản lý nhạc sử dụng PHP mà MySQL
+
+![GitHub language count](https://img.shields.io/github/languages/count/RageOfFire/KingSoundTrackMP3)
+![Discord](https://img.shields.io/discord/752171524919918672)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/RageOfFire/KingSoundTrackMP3)
+![GitHub last commit](https://img.shields.io/github/last-commit/RageOfFire/KingSoundTrackMP3)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/RageOfFire/KingSoundTrackMP3)
+![GitHub repo size](https://img.shields.io/github/repo-size/RageOfFire/KingSoundTrackMP3)
+![GitHub followers](https://img.shields.io/github/followers/RageOfFire)
+![GitHub Repo stars](https://img.shields.io/github/stars/RageOfFire/KingSoundTrackMP3)
+![Website](https://img.shields.io/website?down_color=red&down_message=Offline&up_color=green&up_message=%C4%90ang%20ho%E1%BA%A1t%20%C4%91%E1%BB%99ng&url=https%3A%2F%2Fkingsoundtrackmp3.tk%2F)
+
+## Các chức năng chính
+
+* Đăng ký tài khoản người dùng
+* Chỉnh sửa tài khoản
+* Thêm nhạc
+* Nghe nhạc
+* Chỉnh sửa nhạc
+* Xóa nhạc
+* Quản lý tài khoản,nhạc
+* Mingame
+* chuyển đổi youtube video sang dạng mp3
+* ...
+
+## Bắt đầu
+
+### Yêu cầu
+
+* [Xampp](https://www.apachefriends.org/download.html)
+
+### Cài đặt
+
+* Đưa folder vào htdocs trong [Xampp](https://www.apachefriends.org/download.html)
+* xuất file assets/database/kingsoundtrackmp3.sql vào phpmyadmin
+
+### Chạy
+
+mở file index.php thông qua <http://localhost/KingSoundTrackMP3> để bắt đầu sử dụng
+
+## Hỗ trợ
+
+![GitHub issues](https://img.shields.io/github/issues/RageOfFire/KoraBot)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/RageOfFire/KoraBot)
+
+## Tác giả
+
+* [RageOfFire](https://github.com/RageOfFire)
+
+## License
+
+![GitHub](https://img.shields.io/github/license/RageOfFire/KingSoundTrackMP3)
